d3d1fab6b40dd7fcb78b87e1a028a7097250a1ad
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2026-01-06 17:44:29 +0600

    Add Time class for time representation

--- src/App.java
+++ src/App.java
@@ -1,3 +1,19 @@
+class Time{
+    private int second;
+    private int hour;
+    private int minute;
+    
+    public Time(int hour, int minute, int second){
+        this.hour = hour;
+        this.minute = minute;
+        this.second = second;
+    }
+
+    
+}
+
+
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
