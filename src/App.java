800c185f6d34cd51c462a637cb3a32f1c4f2fd88
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2026-01-06 17:49:41 +0600

    Implement Time class with getters and setters; add example usage in App class

--- src/App.java
+++ src/App.java
@@ -7,9 +7,33 @@ class Time{
         this.hour = hour;
         this.minute = minute;
         this.second = second;
+        
     }
 
-    
+    public int getHour(){
+        return hour;
+    }
+    public void setHour(int hour){
+        this.hour = hour;
+    }
+
+
+    public int getMinute(){
+        return minute;
+    }
+    public void setMinute(int minute){
+        this.minute = minute;
+    }
+
+
+    public int getSecond(){
+        return second;
+    }
+    public void setSecond(int second){
+        this.second = second;
+    }
+
+
 }
 
 
@@ -17,5 +41,10 @@ class Time{
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        Time t1 = new Time(10, 20, 30);
+
+        System.out.println("Hour: " + t1.getHour());
+        System.out.println("Minute: " + t1.getMinute());
+        System.out.println("Second: " + t1.getSecond());
     }
 }
