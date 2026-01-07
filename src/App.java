73e46b30f1f560a369b565eda6fe7e92f92a02b2
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2026-01-07 09:36:23 +0600

    Refactor Time class setters for validation and update example usage in App class

--- src/App.java
+++ src/App.java
@@ -10,25 +10,27 @@ class Time{
         
     }
 
-    public int getHour(){
-        return hour;
-    }
-    public void setHour(int hour){
-        this.hour = hour;
-        if(hour > 0 && hour <= 24){
-             this.hour = hour;
-        }else{
-            System.out.println("Invalid hour value");
-        }
-    }
+   
 
+     public int getSecond(){
+        return second;
+    }
+    public void setSecond(int second){
+        //we can controll the second value between 0-59
+        if(second >= 0 && second < 60){
+            this.second = second;
+        }else{
+            System.out.println("Invalid second value");
+        }
+        
+    }
 
     public int getMinute(){
         return minute;
     }
     public void setMinute(int minute){
-       
-        if(minute > 0 && minute < 60){
+       //we can controll the minute value between 0-59
+        if(minute >= 0 && minute < 60){
              this.minute = minute;
         }else{
             System.out.println("Invalid minute value");
@@ -36,16 +38,16 @@ class Time{
     }
 
 
-    public int getSecond(){
-        return second;
+    public int getHour(){
+        return hour;
     }
-    public void setSecond(int second){
-        if(second > 0 && second < 60){
-            this.second = second;
+    public void setHour(int hour){
+        //we can controll the hour value between 0-23
+        if(hour >= 0 && hour < 24){
+             this.hour = hour;
         }else{
-            System.out.println("Invalid second value");
+            System.out.println("Invalid hour value");
         }
-        
     }
 
 
@@ -56,19 +58,25 @@ class Time{
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        Time t1 = new Time(10, 20, 60);
+        Time t1 = new Time(10, 20, 50);
 
+        
+        System.out.println("Second: " + t1.getSecond());
+        System.out.println("Minute: " + t1.getMinute());
         System.out.println("Hour: " + t1.getHour());
-        System.out.println("Minute: " + t1.getMinute());
-        System.out.println("Second: " + t1.getSecond());
+        
 
-        t1.setHour(55);
+        
+        t1.setSecond(70);
         t1.setMinute(45);
-        t1.setSecond(50);
+        t1.setHour(55);
+
         System.out.println("Updated Time:");
-        System.out.println("Hour: " + t1.getHour());        
+        
+        System.out.println("Second: " + t1.getSecond());      
         System.out.println("Minute: " + t1.getMinute());
-        System.out.println("Second: " + t1.getSecond());
+        System.out.println("Hour: " + t1.getHour());  
+        
 
     }
 }
